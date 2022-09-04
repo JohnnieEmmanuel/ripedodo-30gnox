455e5c9017dadf9f1e0c19494985afd4b59817d7
JohnnieEmmanuel <udomajohn21@gmail.com>
2022-09-04 04:25:39 +0100

    removed some unwanted global css

--- src/app/headers/headers.component.ts
+++ src/app/headers/headers.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ViewheaderComponent } from '../viewheader/viewheader.component';
 @Component({
   selector: 'app-headers',
   templateUrl: './headers.component.html',
