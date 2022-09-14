825a1a50518f9ea01bb27a57a86e8a7c45e3fa18
JohnnieEmmanuel <udomajohn21@gmail.com>
2022-09-14 01:43:18 +0100

    filtering series

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,11 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { CarouselModule } from 'ngx-owl-carousel-o';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilterpipePipe } from './pipes/filterpipe.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ViewheaderComponent],
+  declarations: [AppComponent, ViewheaderComponent, FilterpipePipe],
 imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,  FormsModule, ReactiveFormsModule, 
   HttpClientModule,Ng2SearchPipeModule, CarouselModule,BrowserAnimationsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { MoviecrudService } from '../services/moviecrud.service';
 
-
+import { Injectable } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -11,6 +11,22 @@ import { MoviecrudService } from '../services/moviecrud.service';
 export class Tab1Page {
 
   Movies: any = [];
+  items:any =[
+    {
+        "id": "0",
+        "status":"marketplace",
+        "name": "Webstore",
+        "link": "https://youthscarf.com",
+        "iconlink": "http://atmoweb.id/wp-content/uploads/2019/10/ysweb.jpg"
+    },
+    {
+        "id": "6",
+        "status":"oa",
+        "name": "Instagram Signature",
+        "link": "https://instagram.com/youthcatalog_id/",
+        "iconlink": "http://atmoweb.id/wp-content/uploads/2019/10/listdummy.jpg"
+    }
+]
   
   constructor(private movieCrudService: MoviecrudService) {}
 
@@ -19,7 +35,29 @@ export class Tab1Page {
       this.Movies = response;
       console.log(this.Movies);
 
+    getseries(this.Movies);
+
     })
+
+  
+
   }
+   getMarketplaceItems(items) {
+    return items.filter(item => item.status === 'marketplace');
+  }
+  
 
 }
+function getseries(moviearrs:any) {
+  console.log("manner from heaven")
+  console.log(moviearrs)
+  console.log(moviearrs.filter(moviearr => moviearr.movietype === 'series')) ;
+
+  return moviearrs.filter(moviearr => moviearr.movietype === 'series');
+
+
+
+}
+
+
+
